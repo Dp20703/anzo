a0cf7a3815dc57a32d9c5c980762dd70f17c8772
dp <codewithdp2073@gmail.com>
2024-10-31 19:14:54 +0530

    Page1Bottom compo created

--- src/components/Page1Bottom.jsx
+++ src/components/Page1Bottom.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Page1Bottom = () => {
-  return (
-    <div>Page1Bottom</div>
-  )
+    return (
+        <div className='absolute bottom-0 left-0 w-full flex items-end justify-between p-24 '>
+            <div>
+                <h2 className='text-2xl font-[anzo5]'>BRAND DESIGN | WEBSITE DESIGN</h2>
+                <h3 className='text-2xl font-[anzo3] text-gray-300'>BESPOKE FREELANCE</h3>
+            </div>
+            <div>
+                <img className='mb-10' src="https://static.wixstatic.com/media/f1c650_ed71078001ef401889b71537cca76dc4~mv2.png/v1/fill/w_100,h_100,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cssda-wotd-white.png" alt="" />
+                <img src="https://static.wixstatic.com/media/f1c650_64de2af00f41462e8aecc3d04537f9b4~mv2.png/v1/fill/w_100,h_100,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/EN_legend_large.png" alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Page1Bottom
