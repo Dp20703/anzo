c5cb6f478ecc1dfef4e7925dddd6b0d7f8907ee8
dp <codewithdp2073@gmail.com>
2024-11-01 14:02:46 +0530

    tiltText animation ready

--- src/components/Page1Bottom.jsx
+++ src/components/Page1Bottom.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Page1Bottom = () => {
     return (
-        <div className='absolute bottom-0 left-0 w-full flex items-end justify-between p-24 '>
-            <div>
+        <div className='absolute bottom-0 left-0 w-full flex items-end justify-between p-16 '>
+            <div >
                 <h2 className='text-2xl font-[anzo5]'>BRAND DESIGN | WEBSITE DESIGN</h2>
                 <h3 className='text-2xl font-[anzo3] text-gray-300'>BESPOKE FREELANCE</h3>
             </div>
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import TiltText from '../components/TiltText'
+import React, { useRef, useState } from 'react'
 import Page1Bottom from '../components/Page1Bottom'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Page1 = () => {
+    const tiltRef = useRef(null);
+    const [xVal, setXVal] = useState(0)
+    const [yVal, setYVal] = useState(0)
+
+    //It will called when the mouse move on the page:
+    const mouseMoving = (e) => {
+        setXVal((e.clientX - tiltRef.current.getBoundingClientRect().x - (tiltRef.current.getBoundingClientRect().width / 2)) / 20)
+        setYVal(-(e.clientY - tiltRef.current.getBoundingClientRect().y - (tiltRef.current.getBoundingClientRect().height / 2)) / 8)
+        // console.log(e.clientX,e.clientY)
+        // console.log(tiltRef.current.getBoundingClientRect().x)
+        // console.log(tiltRef.current.getBoundingClientRect().y)
+        // tiltRef.current.style.transform = `rotateX(${yVal}deg) rotateY(${xVal}deg)`
+    }
+    useGSAP(() => {
+        gsap.to(tiltRef.current, {
+            transform: `rotateX(${yVal}deg) rotateY(${xVal}deg)`,
+            duration: 3,
+        })
+    }, [xVal, yVal])
     return (
-        <div className='h-screen bg-white py-4 px-4'>
-            <div className="p-20 h-full bg-cover rounded-[40px] shadow-xl shadow-gray-500 bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1854,h_885,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg)]">
-            <img className='h-24' src="https://static.wixstatic.com/media/f1c650_35a00b1fe6ce403eb237970ec8d34f79~mv2.png/v1/fill/w_99,h_99,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%2053.png" alt="" />
-            <TiltText />
-            <Page1Bottom/>
-        </div>
+        <div className=' h-screen bg-white py-4 px-4 ' onMouseMove={(e) => {
+            mouseMoving(e)
+        }}>
+            <div id='page-in' className=" p-20 h-full bg-cover rounded-[40px] shadow-xl shadow-gray-500 bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1854,h_885,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg)]">
+                <img className='h-24' src="https://static.wixstatic.com/media/f1c650_35a00b1fe6ce403eb237970ec8d34f79~mv2.png/v1/fill/w_99,h_99,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%2053.png" alt="" />
+                <div id='tiltdiv' ref={tiltRef} className='mt-40'>
+                    <h1 className='text-[4.2vw] font-[anzo5] leading-[4vw]'>I AM <span className='text-black font-[anzo1]'>DARK MODE</span>™</h1>
+                    <h1 className='text-[8.2vw] font-[anzo2] leading-[7vw]'>DESIGNER</h1>
+                    <h1 className='text-[4.2vw] font-[anzo5] leading-[4vw]'>TO HIRE</h1>
+                </div>
+                <Page1Bottom />
+            </div>
         </div>
     )
 }
