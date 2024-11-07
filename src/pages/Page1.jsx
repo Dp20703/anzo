b5902f23c64bf4e0a8c420854a41edbb7f331784
dp <codewithdp2073@gmail.com>
2024-11-07 20:22:32 +0530

    title added

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -24,7 +24,7 @@ const Page1 = () => {
         gsap.to(tiltRef.current, {
             transform: `rotateX(${yVal}deg) rotateY(${xVal}deg)`,
             duration: 5,
-            ease:'power4.out',
+            ease: 'power4.out',
         })
     }, [xVal, yVal])
     return (
@@ -33,7 +33,7 @@ const Page1 = () => {
         }}>
             <div id='page-in' className=" p-20 h-full bg-cover rounded-[40px] shadow-xl shadow-gray-500 bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1854,h_885,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg)]">
                 <img className='h-24' src="https://static.wixstatic.com/media/f1c650_35a00b1fe6ce403eb237970ec8d34f79~mv2.png/v1/fill/w_99,h_99,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%2053.png" alt="no-img" />
-               <TiltText tiltRef={tiltRef}/>
+                <TiltText tiltRef={tiltRef} />
                 <Page1Bottom />
             </div>
         </div>
